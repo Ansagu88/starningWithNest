dbaaa469c9315b21b964293ed4a5183908b1e933
Oscar Andres Sanchez Gutierrez <lilthi.services@gmail.com>
2025-09-17 13:58:53 -0300

    Teslo-shop added

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -4,10 +4,13 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
